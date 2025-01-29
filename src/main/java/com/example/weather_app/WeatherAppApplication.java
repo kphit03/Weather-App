1f599994e9334cc569af4cbbb672569993506433
kevin phitsanu <kevinphitsanu22@gmail.com>
2025-01-29 13:19:12 -0600

    Weather App

---
+++ src/main/java/com/example/weather_app/WeatherAppApplication.java
@@ -0,0 +1,12 @@
+package com.example.weather_app;
+// makes this springboot app
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+// makes this springboot app
+@SpringBootApplication
+public class WeatherAppApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(WeatherAppApplication.class, args);
+	}
+
+}
